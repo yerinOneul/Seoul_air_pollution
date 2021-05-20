2d4c989cb4ff523fce3bd2d8c5875a52702cf641
Yerin Hong <ka030221@naver.com>
2021-05-21 06:11:29 +0900

    modify preprocessing

--- seoulAirPollution.py
+++ seoulAirPollution.py
@@ -16,18 +16,30 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 
 
-# In[2]:
+# In[101]:
 
 
 class preprocessing :
-    #year, month, day, hour column들을 날짜형식으로 변경
-    def make_date(year,month,day,hour)
-    #시간별 데이터를 날짜별 데이터로 변경
-    def hour_to_date()
+    #make linear regression model and fit, return linear regression model and score
+    #df : dataframe
+    #columns : columns list for make linear regression model
+    #target : target feature name
+    #test : train test split's test_size parameter
+    #random= : train test split's random_state parameter
+    #return : linear regression model and score
+    def reg_score(self,df,columns,target,test = 0.2,random=0):
+        t_col = columns.copy()
+        t_col.append(target)
+        data = df[t_col]
+        clean_df = data.dropna(how="any")
+        train_X, test_X, train_y, test_y = train_test_split(
+            clean_df[columns],clean_df[target],test_size=test,random_state=random)
+        linear = LinearRegression().fit(train_X,train_y)
+        return linear,linear.score(test_X,test_y)
     
 
 
-# In[3]:
+# In[2]:
 
 
 #*****Data preprocessing*****
@@ -51,7 +63,7 @@ seoul = pd.read_csv("seoul_air_20130301-20170228.csv")
 weather = pd.read_csv("weather.csv")
 
 
-# In[4]:
+# In[3]:
 
 
 #*Data restructuring
@@ -64,101 +76,59 @@ beijing.info()
 beijing.describe()
 
 
-#Drop No column and change date format like seoul dataset
+#Drop No column
 beijing.drop(columns="No",inplace = True)
 
 beijing
 
 
-# In[5]:
+# In[4]:
 
 
+#change year, month, day, hour columns to date format 
 tt = pd.DataFrame(pd.date_range("2013-03-01","2017-03-01",freq="h"))
-
-
-# In[6]:
-
-
 tt = tt[:-1]
-
-
-# In[7]:
-
-
 tt = pd.concat([tt,tt,tt,tt,tt,tt,tt,tt,tt,tt,tt,tt],ignore_index = True)
-
-
-# In[8]:
-
-
 beijing["Date"] = tt
-
-
-# In[9]:
-
-
+#drop year, month, day, hour columns 
 beijing.drop(columns=["year","month","day","hour"],inplace=True)
 
 
-# In[10]:
+# In[5]:
 
 
 beijing
 
 
-# In[11]:
+# In[7]:
 
 
 beijing.info()
+seoul.info()
+weather.info()
+
+
+# In[8]:
+
+
+weather
+seoul
+
+
+# In[9]:
+
+
+#change MSRDT column to date format 
+date_s = seoul["MSRDT"].astype(str)
+seoul["MSRDT"] = date_s.apply(lambda x: datetime.strptime(x, "%Y%m%d%H%M"))
+seoul
 
 
 # In[12]:
 
 
-seoul.info()
-
-
-# In[13]:
-
-
-weather.info()
-
-
-# In[14]:
-
-
-weather
-
-
-# In[15]:
-
-
-seoul
-
-
-# In[16]:
-
-
-date_s = seoul["MSRDT"].astype(str)
-seoul["MSRDT"] = date_s.apply(lambda x: datetime.strptime(x, "%Y%m%d%H%M"))
-
-
-# In[17]:
-
-
-seoul
-
-
-# In[18]:
-
-
 weather["tm"] = weather["tm"].apply(lambda _: datetime.strptime(_,"%Y-%m-%d"))
-
-
-# In[19]:
-
-
-weather["tm"]
+weather.info()
 
 
 # In[20]:
@@ -171,159 +141,112 @@ weather.isnull().sum()
 beijing.isnull().sum()
 
 
-# In[21]:
+# In[14]:
 
 
-#서울 강수량을 0.0으로 fill
+#weather - sumRn이 nan value이면 비가 오지 않은 날이므로 0.0으로 fill
 weather["sumRn"].fillna(0.0,inplace=True)
 
 
-# In[22]:
+# In[15]:
 
 
 weather.isnull().sum()
 
 
-# In[23]:
-
-
-weather  
-
-
-# In[125]:
+# In[16]:
 
 
 #날씨 결측값을 가장 큰 양의 상관관계를 갖는 column을 파악한 뒤 regression으로 모델 학습 후 예측값으로 채우기
 #0.5 정도의 값이 얻어지면 : 강력한 양(+)의 상관. 변인 x 가 증가하면 변인 y 가 증가한다.
-#std_weather = pd.DataFrame(StandardScaler().fit_transform(weather.iloc[:,1:]),columns=weather.iloc[:,1:].columns)
 weather.corr()
-#std_weather.corr()
 
 
-# In[126]:
+# In[17]:
 
 
-#sns.heatmap(std_weather.corr(), annot=True, fmt='0.2f')
+#corr() heatmap
 sns.heatmap(weather.corr(), annot=True, fmt='0.2f')
 plt.title('weather', fontsize=20)
 plt.show()
 
 
-# In[127]:
+# In[104]:
 
 
 #sumGsr과 avgTs는 0.5,sumGsr과 minRhm는 -0.55 sumGsr과 avgTs는 0.4
 #sumGsr-avgTs --> accuray = 0.2..
 #sumGsr-avgTsm,minRhm --> accuray = 0.6
-
-#// maxWd와 sumGsr은 0.2
-#0.2 정도의 값이 얻어지면 : 너무 약해서 의심스러운 양(+)의 상관
-#maxWd는 전날과 다음날의 평균값으로 채운다. 
-
-#df = std_weather[["sumGsr","avgTs","minRhm","avgTa"]]
-df = weather[["sumGsr","avgTs","minRhm","avgTa"]]
-df = df[df["sumGsr"].notnull()]
-
+#sumGsr-avgTsm,minRhm,avgTa --> accuracy = 0.72
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(df[["avgTs","minRhm","avgTa"]],df["sumGsr"],test_size=0.2,random_state=0)
-
-
-# In[128]:
-
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-
-
-# In[129]:
-
-
-#pred_y = linear.predict(std_weather.loc[:, ["avgTs","minRhm","avgTa"]])
+linear,score = preprocessing().reg_score(weather,["avgTs","minRhm","avgTa"],"sumGsr")
+score
 pred_y = linear.predict(weather.loc[:, ["avgTs","minRhm","avgTa"]])
-
-
-# In[130]:
-
-
-#std_weather['sumGsr'].fillna(pd.Series(pred_y), inplace=True)
 weather['sumGsr'].fillna(pd.Series(pred_y), inplace=True)
 
 
-# In[131]:
+# In[105]:
 
 
-#std_weather.isnull().sum()
 weather.isnull().sum()
 
 
-# In[31]:
+# In[20]:
 
 
-#std_weather["Date"] = weather["tm"]
-
-
-# In[132]:
-
-
-#std_weather[std_weather["maxWd"].isnull() == True]
+#// maxWd와 sumGsr은 0.2,
+#0.2 정도의 값이 얻어지면 : 너무 약해서 의심스러운 양(+)의 상관
+# maxWd는 다른 feature들과도 유의미한 상관관계가 나타나지 않으므로 maxWd는 전날 데이터와 다음날의 데이터로 채운다. 
 weather[weather["maxWd"].isnull() == True]
 
 
-# In[133]:
+# In[21]:
 
 
-#std_weather["maxWd"].fillna(method='ffill', limit=1,inplace=True)
 weather["maxWd"].fillna(method='ffill', limit=1,inplace=True)
 
 
-# In[134]:
+# In[22]:
 
 
-#std_weather[std_weather["maxWd"].isnull() == True]
 weather[weather["maxWd"].isnull() == True]
 
 
-# In[135]:
+# In[23]:
 
 
-#std_weather["maxWd"].fillna(method='bfill', limit=1,inplace=True)
 weather["maxWd"].fillna(method='bfill', limit=1,inplace=True)
 
 
-# In[136]:
+# In[24]:
 
 
-#std_weather.isnull().sum()
+#nan value 제거 완료
 weather.isnull().sum()
 
 
 # In[137]:
 
 
+#Aotizhongxin의 결측값
 beijing[beijing["station"]=="Aotizhongxin"].isnull().sum()
 
 
 # In[38]:
 
 
+#beijing 데이터의 총 결측값
 beijing.isnull().sum()
 
 
 # In[39]:
 
 
+#weather와 같은 과정 진행
 beijing.corr()
 
 
-# In[40]:
-
-
-beijing
-
-
-# In[41]:
+# In[25]:
 
 
 sns.heatmap(beijing.corr(), annot=True, fmt='0.2f')
@@ -331,105 +254,81 @@ plt.title('Beijing', fontsize=20)
 plt.show()
 
 
-# In[42]:
+# In[26]:
 
 
-beijing[beijing["PM10"].isnull()]
-
-
-# In[170]:
-
-
-#onehotencoding 
+#categorical value를 encoding, 해당 데이터의 경우 labelencoding보다 onehotencoding이 더 적합
+#onehotencoding for station, wd columns
 ohe = OneHotEncoder()
 station_oh = ohe.fit_transform(beijing["station"].values.reshape(-1,1)).toarray()
 beijing_oh = beijing.copy()
 station_oh = pd.DataFrame(station_oh,columns=beijing["station"].unique())
 
 
-# In[44]:
+# In[27]:
 
 
 station_oh
 
 
-# In[171]:
+# In[28]:
 
 
 beijing_oh.drop(columns="station",inplace=True)
 
 
-# In[172]:
+# In[29]:
 
 
 beijing_oh = pd.concat([beijing_oh,station_oh],axis=1)
 
 
-# In[173]:
+# In[30]:
 
 
-#풍향은 categorical value, numerical value로 변환해야 corr 파악 후 예측할 수 있는데 nan값이 있기 때문에 
-#처리가 난해..
-#풍향은 unique를 통해 max 값으로 채운다. 
-
+#풍향은 categorical value, numerical value로 변환해야 corr 파악 후 예측할 수 있는데 nan값이 있기 때문에 처리가 난해..
+#풍향은 unique를 통해 max 값으로 채운다.
 beijing_oh["wd"].fillna(beijing_oh["wd"].value_counts().index[beijing_oh["wd"].value_counts().argmax()],inplace=True)
 
 
-# In[174]:
+# In[31]:
 
 
 beijing_oh["wd"].isnull().sum()
 
 
-# In[175]:
+# In[32]:
 
 
-#onehotencoding 
+#nan value를 채운 후 onehotencoding 
 wd_oh = ohe.fit_transform(beijing_oh["wd"].values.reshape(-1,1)).toarray()
 wd_oh = pd.DataFrame(wd_oh,columns=beijing_oh["wd"].unique())
-
-
-# In[176]:
-
-
 beijing_oh = pd.concat([beijing_oh,wd_oh],axis=1)
 beijing_oh.drop(columns="wd",inplace=True)
 
 
-# In[161]:
+# In[33]:
 
 
 beijing_oh.columns
 
 
-# In[60]:
+# In[34]:
 
 
-beijing_oh.iloc[:,:11]
+#encoding 후 nan value 다시 측정 
+beijing_oh.isnull().sum()
 
 
-# In[114]:
+# In[35]:
 
 
-#std_beijing = pd.DataFrame(StandardScaler().fit_transform(beijing_oh.iloc[:,:11])
- #                          ,columns =beijing_oh.iloc[:,:11].columns)#날짜 제외 std
-
-
-# In[139]:
-
-
-#std_beijing.isnull().sum()
-#beijing_oh.isnull().sum()
-
-
-# In[142]:
-
-
-#std_beijing.corr()
+#weather와 같은 과정으로 결측값 채우기 진행
+#beijing 데이터의 상관관계 파악
 beijing.corr()
 
 
-# In[143]:
+# In[36]:
 
 
 sns.heatmap(beijing.corr(), annot=True, fmt='0.2f')
@@ -437,182 +336,121 @@ plt.title('Beijing', fontsize=20)
 plt.show()
 
 
-# In[177]:
+# In[111]:
 
 
 #predict PM2.5 - using PM10, NO2, CO 
-#df = std_beijing[["PM2.5","PM10","SO2","NO2","CO"]]
-df = beijing_oh[["PM2.5","PM10","SO2","NO2","CO"]]
-clean_df = df.dropna(how="any")
-
-#linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["PM10","SO2","NO2","CO"]],clean_df["PM2.5"],test_size=0.2,random_state=1)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-
-
-# In[178]:
-
-
-pred_y = linear.predict(clean_df[["PM10","SO2","NO2","CO"]])
-
-#std_beijing["PM2.5"].fillna(pd.Series(pred_y), inplace=True)
+linear,score = preprocessing().reg_score(beijing_oh,["PM10","SO2","NO2","CO"],"PM2.5",0.2,1)
+score
+pred_y = linear.predict(beijing_oh[["PM10","SO2","NO2","CO"]].dropna(how="any"))
 beijing_oh["PM2.5"].fillna(pd.Series(pred_y), inplace=True)
 
 
-# In[179]:
+# In[112]:
 
 
-#std_beijing.isnull().sum() 
 beijing_oh.isnull().sum()#667개의 예측되지 않은 값들
 
 
-# In[180]:
+# In[116]:
 
 
 #predict PM10 -also using PM2.5,SO2 NO2, CO
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["PM2.5","SO2","NO2","CO"]],clean_df["PM10"],test_size=0.2,random_state=2)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-
-pred_y = linear.predict(clean_df[["PM2.5","SO2","NO2","CO"]])
-
+linear,score = preprocessing().reg_score(beijing_oh,["PM2.5","SO2","NO2","CO"],"PM10",0.2,2)
+score
+pred_y = linear.predict(beijing_oh[["PM2.5","SO2","NO2","CO"]].dropna(how="any"))
 beijing_oh["PM10"].fillna(pd.Series(pred_y), inplace=True)
 beijing_oh.isnull().sum()  #478개의 예측되지 않은 값들
 
 
-# In[181]:
+# In[119]:
 
 
 #predict SO2 -also using PM2.5, PM10, NO2, CO
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["PM2.5","PM10","NO2","CO"]],clean_df["SO2"],test_size=0.2,random_state=3)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
+linear,score = preprocessing().reg_score(beijing_oh,["PM2.5","PM10","NO2","CO"],"SO2",0.2,3)
+score
 #낮은 score.. SO2는 regression 대신 median으로 nan 값 채우기
 #std_beijing["SO2"].fillna(std_beijing["SO2"].median(), inplace=True)
 #std_beijing.isnull().sum() 
-beijing_oh["SO2"].fillna(std_beijing["SO2"].median(), inplace=True)
+beijing_oh["SO2"].fillna(beijing_oh["SO2"].median(), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[189]:
+# In[121]:
 
 
 #predict NO2 -also using PM2.5,PM10,SO2,CO
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["PM2.5","PM10","SO2","CO"]],clean_df["NO2"],test_size=0.2,random_state=4)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
+linear,score = preprocessing().reg_score(beijing_oh,["PM2.5","PM10","SO2","CO"],"NO2",0.2,4)
+score
 #낮은 score.. NO2는 regression 대신 median으로 nan 값 채우기
 beijing_oh["NO2"].fillna(beijing_oh["NO2"].median(), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[185]:
+# In[124]:
 
 
 #predict CO -also using PM2.5,PM10,SO2,NO2
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["PM2.5","PM10","SO2","NO2"]],clean_df["CO"],test_size=0.2,random_state=5)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-pred_y = linear.predict(clean_df[["PM2.5","PM10","SO2","NO2"]])
-
+linear,score = preprocessing().reg_score(beijing_oh,["PM2.5","PM10","SO2","NO2"],"CO",0.2,5)
+score
+pred_y = linear.predict(beijing_oh[["PM2.5","PM10","SO2","NO2"]].dropna(how="any"))
 beijing_oh["CO"].fillna(pd.Series(pred_y), inplace=True)
 beijing_oh.isnull().sum()  #901개의 예측되지 않은 값들
 
 
-# In[190]:
+# In[127]:
 
 
 #predict O3 - using NO2,TEMP,PRES
-df = beijing_oh[["O3","NO2","TEMP","PRES","DEWP"]]
-clean_df = df.dropna(how="any")
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["NO2","TEMP","PRES"]],clean_df["O3"],test_size=0.2,random_state=6)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-pred_y = linear.predict(clean_df[["NO2","TEMP","PRES"]])
+linear,score = preprocessing().reg_score(beijing_oh,["NO2","TEMP","PRES"],"O3",0.2,6)
+score
 #낮은 score.. O3는 regression 대신 median으로 nan 값 채우기
-beijing_oh["O3"].fillna(beijing_oh["NO2"].median(), inplace=True)
+beijing_oh["O3"].fillna(beijing_oh["O3"].median(), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[193]:
+# In[129]:
 
 
 #predict TEMP - using O3,PRES,DEWP
-df = beijing_oh[["O3","TEMP","PRES","DEWP"]]
-clean_df = df.dropna(how="any")
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["O3","PRES","DEWP"]],clean_df["TEMP"],test_size=0.2,random_state=7)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-pred_y = linear.predict(clean_df[["O3","PRES","DEWP"]])
+linear,score = preprocessing().reg_score(beijing_oh,["O3","PRES","DEWP"],"TEMP",0.2,7)
+score
+pred_y = linear.predict(beijing_oh[["O3","PRES","DEWP"]].dropna(how="any"))
 beijing_oh["TEMP"].fillna(pd.Series(pred_y), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[195]:
+# In[131]:
 
 
 #predict PRES - using O3,TEMP,DEWP
-df = beijing_oh[["O3","TEMP","PRES","DEWP"]]
-clean_df = df.dropna(how="any")
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["O3","TEMP","DEWP"]],clean_df["PRES"],test_size=0.2,random_state=8)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-pred_y = linear.predict(clean_df[["O3","TEMP","DEWP"]])
+linear,score = preprocessing().reg_score(beijing_oh,["O3","TEMP","DEWP"],"PRES",0.2,8)
+score
+pred_y = linear.predict(beijing_oh[["O3","TEMP","DEWP"]].dropna(how="any"))
 beijing_oh["PRES"].fillna(pd.Series(pred_y), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[198]:
+# In[133]:
 
 
 #predict DEWP - using TEMP,PRES
-df = beijing_oh[["TEMP","PRES","DEWP"]]
-clean_df = df.dropna(how="any")
 #linear regression 
-train_X, test_X, train_y, test_y = train_test_split(clean_df[["TEMP","PRES"]],clean_df["DEWP"],test_size=0.2,random_state=9)
-
-linear = LinearRegression().fit(train_X,train_y)
-linear.coef_
-linear.intercept_
-linear.score(test_X,test_y)
-pred_y = linear.predict(clean_df[["TEMP","PRES"]])
+linear,score = preprocessing().reg_score(beijing_oh,["TEMP","PRES"],"DEWP",0.2,9)
+score
+pred_y = linear.predict(beijing_oh[["TEMP","PRES"]].dropna(how="any"))
 beijing_oh["DEWP"].fillna(pd.Series(pred_y), inplace=True)
 beijing_oh.isnull().sum() 
 
 
-# In[204]:
+# In[134]:
 
 
 #predict RAIN 상관관계가 다 낮음,  ffil,limit=1 and bfil,limit=1
@@ -620,13 +458,13 @@ beijing_oh[beijing_oh["RAIN"].isnull() == True]
 beijing_oh["RAIN"].fillna(method='ffill', limit=1,inplace=True)
 
 
-# In[207]:
+# In[135]:
 
 
 beijing_oh["RAIN"].fillna(method='bfill', limit=1,inplace=True)
 
 
-# In[209]:
+# In[136]:
 
 
 beijing_oh[beijing_oh["RAIN"].isnull() == True]
@@ -648,14 +486,14 @@ beijing_oh[beijing_oh["RAIN"].isnull() == True]
 #지리적 위치가 가까운 구역을 참조하여 nan value 채우기 불가능..
 
 
-# In[257]:
+# In[137]:
 
 
 #연속된 nan value는 median으로 채우기, median = 0.0..
 beijing_oh["RAIN"].fillna(beijing_oh["RAIN"].median(),inplace=True)
 
 
-# In[258]:
+# In[138]:
 
 
 #precit WSPM 상관관계가 다 낮음, ffil,limit=1 and bfil,limit=1
@@ -665,20 +503,20 @@ beijing_oh["WSPM"].fillna(method='bfill', limit=1,inplace=True)
 beijing_oh[beijing_oh["WSPM"].isnull() == True]
 
 
-# In[260]:
+# In[139]:
 
 
 #연속된 nan value는 median으로 채우기, median = 1.4..
 beijing_oh["WSPM"].fillna(beijing_oh["WSPM"].median(),inplace=True)
 
 
-# In[261]:
+# In[140]:
 
 
 beijing_oh.isnull().sum() 
 
 
-# In[263]:
+# In[141]:
 
 
 #남은 nan value - PM2.5 667, PM10 478,CO 901
@@ -687,7 +525,7 @@ beijing_oh[beijing_oh["PM10"].isnull() == True]
 beijing_oh[beijing_oh["CO"].isnull() == True]
 
 
-# In[265]:
+# In[142]:
 
 
 #ffil,limit=1 and bfil,limit=1으로 채우기
@@ -697,14 +535,14 @@ beijing_oh["PM2.5"].fillna(method='bfill', limit=1,inplace=True)
 beijing_oh[beijing_oh["PM2.5"].isnull() == True]
 
 
-# In[267]:
+# In[143]:
 
 
 #연속된 nan value는 median으로 채우기, median = 1.4..
 beijing_oh["PM2.5"].fillna(beijing_oh["PM2.5"].median(),inplace=True)
 
 
-# In[271]:
+# In[144]:
 
 
 #ffil,limit=1 and bfil,limit=1으로 채우기
@@ -716,7 +554,7 @@ beijing_oh[beijing_oh["PM10"].isnull() == True]
 beijing_oh["PM10"].fillna(beijing_oh["PM10"].median(),inplace=True)
 
 
-# In[274]:
+# In[145]:
 
 
 #ffil,limit=1 and bfil,limit=1으로 채우기
@@ -728,9 +566,10 @@ beijing_oh[beijing_oh["CO"].isnull() == True]
 beijing_oh["CO"].fillna(beijing_oh["CO"].median(),inplace=True)
 
 
-# In[275]:
+# In[146]:
 
 
+#결측값 채우기 완료
 beijing_oh.isnull().sum()
 
 
@@ -776,6 +615,99 @@ std_beijing
 mm_beijing
 ma_beijing
 rb_beijing
+
+
+# In[154]:
+
+
+std_weather = pd.DataFrame(StandardScaler().fit_transform(weather.drop(columns="tm")),columns=weather.drop(columns="tm").columns)
+std_weather["tm"]=weather["tm"]
+
+
+# In[158]:
+
+
+mm_weather = pd.DataFrame(MinMaxScaler().fit_transform(weather.drop(columns="tm")),columns=weather.drop(columns="tm").columns)
+mm_weather["tm"]=weather["tm"]
+
+
+# In[159]:
+
+
+ma_weather = pd.DataFrame(MaxAbsScaler().fit_transform(weather.drop(columns="tm")),columns=weather.drop(columns="tm").columns)
+ma_weather["tm"]=weather["tm"]
+
+
+# In[160]:
+
+
+rb_weather = pd.DataFrame(RobustScaler().fit_transform(weather.drop(columns="tm")),columns=weather.drop(columns="tm").columns)
+rb_weather["tm"]=weather["tm"]
+
+
+# In[161]:
+
+
+std_weather
+mm_weather
+ma_weather
+rb_weather
+
+
+# In[164]:
+
+
+#서울 데이터 onehotencoding for MSRSTE_NM
+ohe = OneHotEncoder()
+Nm_oh = ohe.fit_transform(seoul["MSRSTE_NM"].values.reshape(-1,1)).toarray()
+seoul_oh = seoul.copy()
+Nm_oh = pd.DataFrame(Nm_oh,columns=seoul["MSRSTE_NM"].unique())
+Nm_oh
+seoul_oh.drop(columns="MSRSTE_NM",inplace=True)
+seoul_oh = pd.concat([seoul_oh,Nm_oh],axis=1)
+
+
+# In[167]:
+
+
+seoul_oh
+
+
+# In[209]:
+
+
+std_seoul = pd.DataFrame(StandardScaler().fit_transform(seoul_oh.iloc[:,1:7]),columns=seoul_oh.iloc[:,1:7].columns)
+mm_seoul = pd.DataFrame(MinMaxScaler().fit_transform(seoul_oh.iloc[:,1:7]),columns=seoul_oh.iloc[:,1:7].columns)
+ma_seoul = pd.DataFrame(MaxAbsScaler().fit_transform(seoul_oh.iloc[:,1:7]),columns=seoul_oh.iloc[:,1:7].columns)
+rb_seoul = pd.DataFrame(RobustScaler().fit_transform(seoul_oh.iloc[:,1:7]),columns=seoul_oh.iloc[:,1:7].columns)
+
+
+# In[210]:
+
+
+col = seoul_oh.iloc[:,1:7].columns.append(seoul_oh.drop(columns=std_seoul.columns).columns)
+
+
+# In[211]:
+
+
+std_seoul = pd.concat([std_seoul,seoul_oh.drop(columns=std_seoul.columns)],axis=1,ignore_index = True)
+mm_seoul = pd.concat([mm_seoul,seoul_oh.drop(columns=mm_seoul.columns)],axis=1,ignore_index = True)
+ma_seoul = pd.concat([ma_seoul,seoul_oh.drop(columns=ma_seoul.columns)],axis=1,ignore_index = True)
+rb_seoul = pd.concat([rb_seoul,seoul_oh.drop(columns=rb_seoul.columns)],axis=1,ignore_index = True)
+std_seoul.columns=col
+mm_seoul.columns=col
+ma_seoul.columns=col
+rb_seoul.columns=col
+
+
+# In[214]:
+
+
+std_seoul
+mm_seoul
+ma_seoul
+rb_seoul
 
 
 # In[275]:
